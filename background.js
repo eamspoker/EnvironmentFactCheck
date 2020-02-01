0d101a2329f5feda021a8bf4e58f5172d1a57605
Emily Amspoker <eamspoker21@kentdenver.org>
2020-02-01 10:33:32 -0500

    Working on getting popup to work with word replacement

--- background.js
+++ background.js
@@ -1,5 +1,17 @@
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: 'wordsss'}, function() {
-    console.log("The color is green.");
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log(document.location.host);
   });
+
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    console.log(document.location.host);
+     chrome.declarativeContent.onPageChanged.addRules([{
+       conditions: [new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {hostEquals: 'www.w3schools.com'},
+        })
+        ],
+           actions: [new chrome.declarativeContent.ShowPageAction()]
+     }]);
+   });
+
 });
--- content.js
+++ content.js
@@ -2,7 +2,7 @@ var elementsInsideBody = [...document.body.getElementsByTagName('*')];
 // This makes an array of everything inside the body tag
 
 //importing file system
-let urls = ["", "Americasfreedomfighters.com", "Rightwingtribune.com.", "Americaslastlineofdefense.org", "worstpot.us", "nofakenewsonline.us", "Theamericanews.co", "Americanjournalreview.com", "Americatalks.com", "thepedogate.com", "Americatalks.com:", "Americatalks.com", "BannedInformation.com", "BannedInformation.com", "BannedInformation.com", "FBnewscycle.com.", "BlingNews.com", "BlingNews.com", "CBINFO24.com", "Cbinfo24.com", "Channel23News.com", "Conservativeangle.com", "Consnation.com", "Conservativepost.com", "Theconservativetreehouse.com", "Daily-vine.com", "empirenews.net", "En-volve.com", "Fbnewscycle.com", "FBNewsCycle.com", "FBNewsCycle.com", "BannedInformation.com.", "Fellowshipoftheminds.com", "Gellerreport.com", "Infowars.com", "KAGfeed.com", "Thelibertyraise.com", "Londonwebnews.com", "MMinfo24.com", "MMinfo24.com", "Neonnettle.com", "Newspunch.com", "Newslo/Politicops.com", "politicops.com", "politicops.com", "NYeveningnews.com", "Patriotswalk.us", "Policetask.com", "Politicsfocus.com", "The-postillon.com", "Puppetstringnews.com", "Realnewsrightnow.com", "Rightwingtribune.com", "Americasfreedomfighters.com.", "RWNofficial.com", "Therightists.com", "SpecialNewsUSA.com", "7newspolitical.site", "Stgeorgegazette.com", "Stuppid.com", "Stuppid.com", "Teddystick.com", "Topalertnews.com", "Truthfeednews.com", "Universaleinfo.com", "Trainnews.info", "Ussanews.com", "Viralcords.com", "Viralnewspbs.site", "Viralitythings.us", "WebViners.com", "Webviners.com", "Worldnews-24.press"];
+let urls = ["Developer.Chrome.com", "App.Slack.com", "Americasfreedomfighters.com", "Rightwingtribune.com.", "Americaslastlineofdefense.org", "worstpot.us", "nofakenewsonline.us", "Theamericanews.co", "Americanjournalreview.com", "Americatalks.com", "thepedogate.com", "Americatalks.com:", "Americatalks.com", "BannedInformation.com", "BannedInformation.com", "BannedInformation.com", "FBnewscycle.com.", "BlingNews.com", "BlingNews.com", "CBINFO24.com", "Cbinfo24.com", "Channel23News.com", "Conservativeangle.com", "Consnation.com", "Conservativepost.com", "Theconservativetreehouse.com", "Daily-vine.com", "empirenews.net", "En-volve.com", "Fbnewscycle.com", "FBNewsCycle.com", "FBNewsCycle.com", "BannedInformation.com.", "Fellowshipoftheminds.com", "Gellerreport.com", "Infowars.com", "KAGfeed.com", "Thelibertyraise.com", "Londonwebnews.com", "MMinfo24.com", "MMinfo24.com", "Neonnettle.com", "Newspunch.com", "Newslo/Politicops.com", "politicops.com", "politicops.com", "NYeveningnews.com", "Patriotswalk.us", "Policetask.com", "Politicsfocus.com", "The-postillon.com", "Puppetstringnews.com", "Realnewsrightnow.com", "Rightwingtribune.com", "Americasfreedomfighters.com.", "RWNofficial.com", "Therightists.com", "SpecialNewsUSA.com", "7newspolitical.site", "Stgeorgegazette.com", "Stuppid.com", "Stuppid.com", "Teddystick.com", "Topalertnews.com", "Truthfeednews.com", "Universaleinfo.com", "Trainnews.info", "Ussanews.com", "Viralcords.com", "Viralnewspbs.site", "Viralitythings.us", "WebViners.com", "Webviners.com", "Worldnews-24.press"];
 
 
 let fullname = document.location.host;
@@ -37,8 +37,6 @@ function replaceText (node) {
   node.nodeValue = value;
 }
 
-let changeColor = document.getElementById('changeColor');
-
 chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
   changeColor.setAttribute('value', data.color);
@@ -53,4 +51,4 @@ changeColor.onclick = function(element) {
     });
   };
 
-//window.onload = fakeCheck();
+window.onload = popup();
